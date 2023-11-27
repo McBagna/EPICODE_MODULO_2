3b8f334a20f64b20c9b115713c2dcaac4defd943
McBagna <117860111+McBagna@users.noreply.github.com>
2023-11-27 20:02:11 +0100

    Update W5D4_Pratica.c

--- W5D4_Pratica.c
+++ W5D4_Pratica.c
@@ -6,8 +6,10 @@ int main ()
     double lato;
 
     printf("Inserisci un numero reale: \n");
+    // Qui sotto verifichiamo che abbiano davvero inserito un numero
     if (scanf("%lf", &lato) == 1)
     {
+        // Non salviamo nelle variabili ma stampiamo direttamente cosi risparmiamo memoria
         printf("L'area del quadrato è: %lf \n",lato*lato);
         printf("L'area del cerchio è: %lf \n",(lato/2.0) * (lato/2.0) * M_1_PI);
         printf("L'area del triangolo equilatero è: %lf \n",(sqrt(3) / 4) * lato * lato);
